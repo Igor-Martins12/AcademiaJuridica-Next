21e3677ae79f502046e4344bc1788c8074dd5506
Igor-Martins12 <igorjose07@hotmail.com>
2023-11-06 11:21:02 -0300

    "Sidebar-navbar"

---
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,9 @@
+const SearchPage = () => {
+    return ( 
+        <div>
+            this is a search Page!
+        </div>
+     );
+}
+ 
+export default SearchPage;
--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,9 +1,32 @@
 "use client";
 
+import{ Compass, Layout } from  "lucide-react";
+import { SidebarItem } from "./sidebar-item";
+const guestRoutes = [
+        {
+            icon: Layout,
+            label: "Dashboard",
+            href: "/",
+        },
+        {
+            icon: Compass,
+            label: "Browse",
+            href: "/search",
+        },
+    ] 
  export const SidebarRoutes = () => {
+    const routes = guestRoutes;
+
     return ( 
-        <div>
-            Routes!
+        <div className="flex flex-col w-full">
+            {routes.map((route) => (
+                <SidebarItem 
+                    key={route.href}
+                    icon={route.icon}
+                    label={route.label}
+                    href={route.href}
+                />
+            ))}
         </div>
      )
 }
