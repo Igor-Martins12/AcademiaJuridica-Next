76f926bb945b2a64cbbc90a93471a6cf963cc810
Igor-Martins12 <igorjose07@hotmail.com>
2023-11-14 13:04:49 -0300

    "adicionandoPenal"

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -4,19 +4,28 @@ const database = new PrismaClient();
 
 async function main() {
     try { 
-       await database.category.createMany({
-        data: [
-            {name: "Pós graduação em direito previdenciario"},
-            {name: "Direito do Consumidor"},
-            {name: "Direito Trabalhista"},
-            {name: "Direito Contratual"},
-            {name: "Direito Civil"},
-            {name: "Direito Ambiental"},
-            {name: "Direito Empresarial"},
-            {name: "Direito da Tecnologia da Informação"},
-        ]
-       });
-       console.log("Sucesso")
+        const categories = [
+            "Pós graduação em Direito Previdenciario",
+            "Direito do Consumidor",
+            "Direito Trabalhista",
+            "Direito Contratual",
+            "Direito Civil",
+            "Direito Ambiental",
+            "Direito Empresarial",
+            "Direito da Tecnologia da Informação",
+            "Direito Penal",
+        ];
+        for (const name of categories) {
+            const existingCategory = await database.category.findUnique({
+                where: { name },
+            });
+
+            if (!existingCategory) {
+                await database.category.create({ data: { name } });
+            }
+        }
+
+        console.log("Sucesso");
     } catch (error) {
         console.log("Erro ao propagar as categorias do banco de dados", error);
     } finally {
