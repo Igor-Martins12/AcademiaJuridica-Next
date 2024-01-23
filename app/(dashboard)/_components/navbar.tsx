756c652a3b5a322f781e9d6b0fe7a0ebe93f228f
Igor Martins <igorjose07@hotmail.com>
2024-01-23 09:38:34 -0300

    "teste"

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -3,12 +3,12 @@ import { MobileSidebar } from "./mobile-sidebar";
 
 
 
-  export const Navbar = () => {
-    return ( 
-     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
-        <MobileSidebar />
-        <NavbarRoutes />
-    </div> 
-    );
+export const Navbar = () => {
+  return (
+    <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+      <MobileSidebar />
+      <NavbarRoutes />
+    </div>
+  );
 }
- 
+
