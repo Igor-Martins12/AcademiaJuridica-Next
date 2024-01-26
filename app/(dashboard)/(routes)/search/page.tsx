72874f6e4a1cd38d4ad593dfca5da46d3852d793
Igor Martins <igorjose07@hotmail.com>
2024-01-26 06:31:25 -0300

    "cardCurso"

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -34,7 +34,7 @@ const courses = await getCourses({
             <div className="px-6 pt-6 md:hidden md:mb-0 block">
                 <SearchInput />
             </div>
-            <div className="p-6">
+            <div className="p-6 space-y-4">
             <Categories
             items={categories}
             />
